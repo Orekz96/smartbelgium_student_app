e83dd8b55f1c6a26515fabd0c4e6faba67a1cfe3
Richard Orekoya <orekoyadare12@gmail.com>
2025-06-29 23:56:00 +0100

    Add about section and Our Services section, with basic stylings

---
+++ app.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  //Set the carousel options
+  $("#quote-carousel").carousel({
+    pause: true,
+    interval: 4000,
+  });
+});
+
+console.log("Carousel initialized");
+const a = 2;
+const b = 3;
+const sum = a + b;
+console.log(`The sum of ${a} and ${b} is ${sum}`);
